f333d371b092537752ed3750bb729f46b9dc92da
salidamaharjan <salidamaharjan@outlook.com>
2023-11-15 23:43:08 -0600

    using confirm method to ask the user to include the character and storing in a variable

--- script.js
+++ script.js
@@ -7,13 +7,23 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-//Creating generatePassword function;
+//Creating generatePassword function
 function generatePassword () {
   //capturing the user input in a variable.
   var userInput =  prompt("Create password? Select number between 8 and 128.");
+  //if condition to check the userInput less than 8 and greater than 128 and executing the alert accordingly.
   if ( userInput < 8 ) {
     alert("Please select a number greater than 8!!");
   }
+  if (userInput > 128 ) {
+    alert("Please select a number less than 128!!");
+  }
+//Giving option to the user to include the given characters
+  var includeNum = confirm("Do you want to include number?");
+  var includeSpecialChar = confirm("Do you want to include special character?");
+  var includeUpperCase = confirm("Do you want to include upper case letter?");
+  var includesLowerCase = confirm("Do you want to include lower case letter?");
+  return null;
 }
 
 // Add event listener to generate button
