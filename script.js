03b35d13a05e9157274ae5f0a0a067193ee15bfc
salidamaharjan <salidamaharjan@outlook.com>
2023-11-15 23:24:46 -0600

    using prompt method and taking user input

--- script.js
+++ script.js
@@ -5,10 +5,14 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
-
+//Creating generatePassword function;
+function generatePassword () {
+  prompt("Create password? Select number between 8 and 128.");
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// returns string
+
